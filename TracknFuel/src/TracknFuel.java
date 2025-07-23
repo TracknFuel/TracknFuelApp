d128042f8efe4bdd80f4b42cd21f802f24615d5e
TracknFuel <tracknfuel2@gmail.com>
2025-07-23 23:46:09 +0200

    Initial project commit from NetBeans

--- TracknFuel/src/TracknFuel.java
+++ TracknFuel/src/TracknFuel.java
@@ -3,6 +3,7 @@ public class TracknFuel {
     public static void main(String[] args) {
         System.out.print("Hellow 19");
         System.out.println("\nApd students");
+        System.out.print("\nGroup 19");
     }
     
 }
